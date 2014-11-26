56c86ba44169a9ff55606a6fdf0aee2386278a5d
Deathridge <eyes.of.daniel@gmail.com>
2014-11-26 02:24:53 +0000

    changes

---
+++ blog/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import patterns, include, url
+from django.views.generic import ListView
+from blog.models import Post
+
+urlpatterns = patterns('',
+						url(r'^', ListView.as_view(
+								queryset=Post.objects.all().order_by("-created_at")[:10],
+								template_name="blog.html")),
+
+)
