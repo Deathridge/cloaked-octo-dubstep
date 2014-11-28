bea1c73992c8952b9c9be58b0a71b109ba2e12c3
Deathridge <eyes.of.daniel@gmail.com>
2014-11-28 07:25:45 +0000

    Removal of slug field as was not needed

--- blog/models.py
+++ blog/models.py
@@ -1,19 +1,12 @@
 from django.db import models
-from django.template.defaultfilters import slugify
 
 # Create your models here.
 class Post(models.Model):
     created_at = models.DateTimeField(auto_now_add=True, editable=False)
     updated_at = models.DateTimeField(auto_now=True, editable=False)
-    title = models.CharField(max_length = 140)
-    slug = models.SlugField(max_length=255,blank=True, default='')
+    title = models.CharField(max_length = 140)   
     body = models.TextField()
     published = models.BooleanField(default=True)
 
     def __unicode__(self):
         return self.title
-
-    def save(self, *args, **kwargs):
-        if not self.slug:
-            self.slug = slugify(self.title)
-        super(Post, self).save(*args,**kwargs)
